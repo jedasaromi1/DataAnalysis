c817697db229fbd03728ce2b7528ac87b9793061
jedasaromi1 <49847170+jedasaromi1@users.noreply.github.com>
2021-04-13 01:17:01 +0100

    Create test_master.py

---
+++ test_master.py
@@ -0,0 +1,11 @@
+## perform basic calculation
+
+#creating a object for sales and cost price
+sales_price = 2000
+cost_price =1500
+
+#using the created object for calculation
+profit = sales_price-cost_price
+
+#using strings to display the profit
+print("The profit made is: ",profit)
